10183a094c7b4d90d47ad8b8c44ac68f04596bd2
pro0o <pp48041721@student.ku.edu.np>
2024-06-15 01:42:52 +0545

    feat: chunk mesh.

---
+++ world_objects/chunk.py
@@ -0,0 +1,30 @@
+from settings import *
+from meshes.chunk_mesh import ChunkMesh
+
+class Chunk:
+    def __init__(self, app):
+        self.app = app
+        # array of voxels
+        self.voxels: np.array = self.build_voxels()
+        self.mesh: ChunkMesh = None
+        self.build_mesh()
+
+    def build_mesh(self):
+        self.mesh = ChunkMesh(self)
+
+    def render(self):
+        self.mesh.render()
+
+    def build_voxels(self):
+        # initially the voxels will be empty space i.e. 0 in the range of [0-255]
+        voxels = np.zeros(CHUNK_VOL, dtype='uint8')
+
+        # we use 1d array instead of 3d array, for performance boost
+        # so 3d to 1d conversion = x + size*z + area*y
+
+        # filling the chunk with different voxel ranges/
+        for x in range(CHUNK_SIZE):
+            for z in range(CHUNK_SIZE):
+                for y in range(CHUNK_SIZE):
+                    voxels[x + CHUNK_SIZE * z + CHUNK_AREA * y] = 1
+        return voxels
