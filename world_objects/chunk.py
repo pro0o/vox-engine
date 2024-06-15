7d1a88f5ca6abbdf5332f5e39e89fe43ade2607c
pro0o <pp48041721@student.ku.edu.np>
2024-06-15 16:04:31 +0545

    enhance: simple terrain gen.

--- world_objects/chunk.py
+++ world_objects/chunk.py
@@ -30,15 +30,24 @@ class Chunk:
         # initially the voxels will be empty space i.e. 0 in the range of [0-255]
         voxels = np.zeros(CHUNK_VOL, dtype='uint8')
 
+        # determining the world coordinatees relative to voxel
+        cx, cy, cz = glm.ivec3(self.position) * CHUNK_SIZE
+
         # we use 1d array instead of 3d array, for performance boost
         # so 3d to 1d conversion = x + size*z + area*y
 
         # filling the chunk with different voxel ranges/
         for x in range(CHUNK_SIZE):
             for z in range(CHUNK_SIZE):
-                for y in range(CHUNK_SIZE):
-                    voxels[x + CHUNK_SIZE * z + CHUNK_AREA * y] = (
-                        # 3d noise with simplex func
-                        x + y + z if int(glm.simplex(glm.vec3(x,y,z) * 0.1)+1) else 0
-                    )
+                wx = x + cx
+                wz = z + cz
+
+                # maybe height of terrain??
+                world_height = int(glm.simplex(glm.vec2(wx, wz) * 0.01) * 32 + 32)
+                local_height = min(world_height - cy, CHUNK_SIZE) # for current chunk
+
+                for y in range(local_height):
+                    wy = y + cy
+                    voxels[x + CHUNK_SIZE * z + CHUNK_AREA * y] = wy + 1
+        
         return voxels
